4db21adbe88c52dcadf249829c64efb61761cdf4
dancun616 <dancun616@gmail.com>
2023-02-10 14:58:23 +0300

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+  def name=(result)
+   @name = result
+  end
 
+  def name
+    @name
+   end
+
+  def breed=(data)
+   @breed = data
+  end
+
+   def breed 
+    @breed
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+  def name=(person)
+    @name = person
+end
 
+def name
+    @name
+end
+
+def job=(occupation)
+    @job = occupation
+end
+
+def job 
+    @job
+end
 end
